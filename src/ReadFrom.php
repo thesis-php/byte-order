87c575d19da83aa01fc6037f8078eb955fdcd69b
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2026-04-06 20:22:01 +0300

    Use `Order` int type aliases for `ReadFrom` return types and `WriteTo` params

--- src/ReadFrom.php
+++ src/ReadFrom.php
@@ -11,38 +11,51 @@ use Thesis\Endian\Order;
 
 /**
  * @api
+ *
+ * @phpstan-import-type Int8 from Order
+ * @phpstan-import-type Uint8 from Order
+ * @phpstan-import-type Int16 from Order
+ * @phpstan-import-type Uint16 from Order
+ * @phpstan-import-type Int32 from Order
+ * @phpstan-import-type Uint32 from Order
  */
 interface ReadFrom extends Reader
 {
     /**
+     * @return Int8
      * @throws UnexpectedEof
      */
     public function readInt8(Order $order = Order::Network): int;
 
     /**
      * @return non-negative-int
+     * @return Uint8
      * @throws UnexpectedEof
      */
     public function readUint8(Order $order = Order::Network): int;
 
     /**
+     * @return Int16
      * @throws UnexpectedEof
      */
     public function readInt16(Order $order = Order::Network): int;
 
     /**
      * @return non-negative-int
+     * @return Uint16
      * @throws UnexpectedEof
      */
     public function readUint16(Order $order = Order::Network): int;
 
     /**
+     * @return Int32
      * @throws UnexpectedEof
      */
     public function readInt32(Order $order = Order::Network): int;
 
     /**
      * @return non-negative-int
+     * @return Uint32
      * @throws UnexpectedEof
      */
     public function readUint32(Order $order = Order::Network): int;
