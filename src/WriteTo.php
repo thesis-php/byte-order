d382e27c062c49089d4e151db65b1629631dec91
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2026-04-09 01:30:54 +0300

    Widen `WriteTo::writeInt*($v)` param types to `int`

--- src/WriteTo.php
+++ src/WriteTo.php
@@ -11,48 +11,35 @@ use Thesis\Endian\Order;
 
 /**
  * @api
- *
- * @phpstan-import-type Int8 from Order
- * @phpstan-import-type Uint8 from Order
- * @phpstan-import-type Int16 from Order
- * @phpstan-import-type Uint16 from Order
- * @phpstan-import-type Int32 from Order
- * @phpstan-import-type Uint32 from Order
  */
 interface WriteTo extends Writer
 {
     /**
-     * @param Int8 $v
      * @throws WriteFailed
      */
     public function writeInt8(int $v, Order $order = Order::Network): self;
 
     /**
-     * @param Uint8 $v
      * @throws WriteFailed
      */
     public function writeUint8(int $v, Order $order = Order::Network): self;
 
     /**
-     * @param Int16 $v
      * @throws WriteFailed
      */
     public function writeInt16(int $v, Order $order = Order::Network): self;
 
     /**
-     * @param Uint16 $v
      * @throws WriteFailed
      */
     public function writeUint16(int $v, Order $order = Order::Network): self;
 
     /**
-     * @param Int32 $v
      * @throws WriteFailed
      */
     public function writeInt32(int $v, Order $order = Order::Network): self;
 
     /**
-     * @param Uint32 $v
      * @throws WriteFailed
      */
     public function writeUint32(int $v, Order $order = Order::Network): self;
